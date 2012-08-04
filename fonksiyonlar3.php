e9eaf0e4cec24b8f0e51e7f78dfe5f86d0e568f0
mertnesvat <mertnesvat@gmail.com>
2012-08-05 01:00:33 +0300

    Son düzeltmeler eklenmedi
    id numaralarından seçim eklenecek

---
+++ fonksiyonlar3.php
@@ -0,0 +1,69 @@
+<?php
+function duyuru_oku($kacinci) {
+    $url = "http://bilgisayar.kocaeli.edu.tr/";
+    $icerik = file_get_contents("http://bilgisayar.kocaeli.edu.tr/");
+
+    //echo '======================<br>'.$icerik.'<br>==================';
+
+    //basliklar seciliyor.
+    $icerik = str_replace('<a href="files/', '<a href="http://bilgisayar.kocaeli.edu.tr/files/', $icerik);
+    //echo $icerik;
+    preg_match_all("/class=\"popup\">(.*?)<font/", $icerik, $secilen_baslik);
+    preg_match_all("/Yayınlayan <\/th><td>\s*(.*?)<\/td>/", $icerik, $secilen_yayinlayan);
+    preg_match_all("/Konu <\/th><td>\s*(.*?)<\/td>/", $icerik, $secilen_konu);
+    preg_match_all("/Tarih <\/th><td>\s*(.*?)<\/td>/", $icerik, $secilen_tarih);
+    preg_match_all("/Açıklama <\/th><td>\s*(.*?)<\/table>/s", $icerik, $secilen_aciklama);
+    trim($secilen_aciklama[1][$kacinci]);// aciklama icin sondan td tr yi kesmek lazim
+    $secilen_aciklama[1][$kacinci] = str_replace('</td></tr>','', $secilen_aciklama[1][$kacinci]);
+    //$gonderilecek = array('baslik' => $secilen_baslik[1][$kacinci], 'yayinlayan' => $secilen_yayinlayan[1][$kacinci], 'konu' => $secilen_konu[1][$kacinci], 'tarih' => $secilen_tarih[1][$kacinci], 'aciklama' => $secilen_aciklama[1][$kacinci], );
+    
+    $secilen_aciklama[1][$kacinci] = str_replace("<a href=", '', $secilen_aciklama[1][$kacinci]);
+    print_r($secilen_aciklama);
+    echo $secilen_aciklama[1][$kacinci];
+
+    //preg_match_all("/<a href=\"(.*?)\">/", $secilen_aciklama[1][$kacinci],$adresimiz);
+   
+    
+    $gonderilecek = array($secilen_baslik[1][$kacinci],
+    $secilen_yayinlayan[1][$kacinci],
+    $secilen_konu[1][$kacinci],
+    $secilen_tarih[1][$kacinci],
+    $secilen_aciklama[1][$kacinci], );
+
+    return $gonderilecek;
+}
+
+function duyuru_yaz($duyuru,$sayi) {
+    $simdiki = fopen('./duyurular/' .$sayi. '.txt', "w+");
+    $yazilacak = implode(':MN:', $duyuru);
+    $yazilacak = str_replace("&nbsp;", '', $yazilacak);
+    $yazilacak = str_replace("&nbsp", '', $yazilacak);
+    fwrite($simdiki, $yazilacak);
+    fclose($simdiki);
+}
+function dosya_isim_degis($okudum,$yazdim)
+{
+    $degisecek = fopen('./duyurular/'.$okudum.'.txt', "r+");
+    $degisen = fopen('./duyurular/'.$yazdim.'.txt', "w+");
+    $icerik = fread($degisecek, 1024);
+    fwrite($degisen, $icerik, strlen($icerik));
+    fclose($okudum);
+    fclose($yazdim);
+}
+function duyuru_yaz_isimle($duyuru,$dosya_ismi) {
+    
+    $simdiki = fopen('./duyurular/'.$dosya_ismi.'.txt', "w+");
+    $yazilacak = implode(':MN:', $duyuru);
+    fwrite($simdiki, $yazilacak);
+    fclose($simdiki);
+}
+function dosyadan_oku($dosya_ismi){
+
+    $simdiki = fopen('./duyurular/'.$dosya_ismi.'.txt', "r");// sonradan tekrar duzeltildi 20 temmuz 2012
+    $icerik = fread($simdiki, 1024);
+    $gonderilecek = explode(":MN:", $icerik);
+    //print_r($gonderilecek);
+return $gonderilecek;
+}
+
+?>
---
+++ index3.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="tr-TR">
+	<html>
+		<head>
+			<title>duyurular</title>
+			<meta http-equiv="Content-Type" content="text/HTML; charset=utf-8" />
+		</head>
+		<body>
+			<?php
+			echo "==================================<br>&nbsp;&nbsp;Mert Neşvat 2012 =====================<br>==================================<br>";
+			require_once("fonksiyonlar3.php");
+			$last_attention = dosyadan_oku("son_duyuru.txt");
+            $gonderilecek = array_fill(0, 10, "banana");
+            $from = "kayhantolga@letscoding.com";
+            $headers = "From:" . $from;
+            //asthma982loafer@m.facebook.com
+            /* mail('kocaeliduyuru@gmail.com','Baslik : '.$last_attention[0].'Yayinlayan : '.$last_attention[1].'Konu : '.$last_attention[2].'Tarih : '.$last_attention[3].'Aciklama : '.$last_attention[4]
+            ,':)'); */
+            echo '<br> Son Duyuru = '.$last_attention[0];
+			for($i = 0 ; $i < 10 ; $i++)
+            {
+                $attention = duyuru_oku($i);
+                echo '<br> Okunan Duyuru = '.$attention[0];
+                //duzeltmeler
+                for($n = 0 ;$n<5;$n++)
+                {
+                    $attention[$n] = strip_tags($attention[$n]);
+                    $attention[$n] = str_replace("&nbsp;", '', $attention[$n]);                
+                    $attention[$n] = str_replace("&nbsp", '', $attention[$n]);                
+                    $attention[$n] = str_replace("nbsp", '', $attention[$n]);                
+                }
+                
+                if( !strcmp($last_attention[0], $attention[0]) && !strcmp($last_attention[1], $attention[1]) )
+                {
+                    break;
+                }
+                
+                
+                
+                $gonderilecek[$i] = $attention ;
+                
+                
+                mail('kocaeliduyuru@gmail.com','Baslik : '.$attention[0].'Yayinlayan : '.$attention[1].'Konu : '.$attention[2].'Tarih : '.$attention[3].'Aciklama : '.$attention[4]
+                ,':)');
+                
+            }
+            echo 'i = '.$i;
+            if($i == 0)die("<br><b> Simdilik Yeni Duyuru Yok!</b>");
+            for($m = $i ; $m >=0 ; $m--)
+            {
+                duyuru_yaz($gonderilecek[$m],$m);
+            }
+            duyuru_yaz_isimle($gonderilecek[0],"son_duyuru.txt");
+			unset($last_attention);
+            unset($attention);
+echo "<br><br><br><br><br><br>all faq goes to info[at]mertnesvat.com";
+			?>
+		</body>
+	</html>
